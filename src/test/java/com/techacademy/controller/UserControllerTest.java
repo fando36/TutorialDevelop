528ef8d5da29023803c35b4da5efbe1922777142
fumi36 <realist0705@gmail.com>
2025-01-30 21:54:20 +0900

    テストプログラムの修正

--- src/test/java/com/techacademy/controller/UserControllerTest.java
+++ src/test/java/com/techacademy/controller/UserControllerTest.java
@@ -49,23 +49,14 @@ public class UserControllerTest {
         User user1 = new User();
         user1.setId(1);
         user1.setName("キラメキ太郎");
-        user1.setGender(Gender.男性);
-        user1.setAge(27);
-        user1.setEmail("taro.kirameki@mail.com");
 
         User user2 = new User();
         user2.setId(2);
         user2.setName("キラメキ次郎");
-        user2.setGender(Gender.男性);
-        user2.setAge(22);
-        user2.setEmail("jiro.kirameki@mail.com");
 
         User user3 = new User();
         user3.setId(3);
         user3.setName("キラメキ花子");
-        user3.setGender(Gender.女性);
-        user3.setAge(25);
-        user3.setEmail("hanako.kirameki@mail.com");
 
         userList = Arrays.asList(user1, user2, user3);
         when(userService.getUserList()).thenReturn(userList);
@@ -79,5 +70,17 @@ public class UserControllerTest {
                 .andExpect(model().attribute("userlist", hasSize(3))) // userlistの件数が3件であること
                 .andExpect(model().hasNoErrors()) // Modelにエラーが無いこと
                 .andExpect(view().name("user/list")); // viewの名前が "user/list" であること
+
+        // Modelのデータを取得して検証
+        when(model.getAttribute("userlist")).thenReturn(userList);
+        List<User> retrievedUsers = (List<User>) model.getAttribute("userlist");
+        assertThat(retrievedUsers).isNotNull();
+        assertThat(retrievedUsers).hasSize(3);
+
+        // userlist の各要素を検証（1件ずつ取り出してチェック）
+        for (int i = 0; i < userList.size(); i++) {
+            assertThat(retrievedUsers.get(i).getId()).isEqualTo(userList.get(i).getId());
+            assertThat(retrievedUsers.get(i).getName()).isEqualTo(userList.get(i).getName());
+        }
     }
 }
